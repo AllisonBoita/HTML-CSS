f11af775457e3c2537c83000743c2f146780027f
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-03-11 08:11:24 -0300

    FUNCTION: Retornando valores

--- 13_JAVASCRIPT/FUNÇÕES/index.js
+++ 13_JAVASCRIPT/FUNÇÕES/index.js
@@ -3,3 +3,16 @@ function boasVindas (nome, sobrenome){
 }
 
 boasVindas('Allison', 'Boita')
+
+/* Return */
+
+const a = 10
+const b = 20
+const c = 29
+
+function soma(n1, n2, n3) {
+    return n1 + n2 + n3
+}
+
+const resultado = soma(a,b,c)
+console.log(resultado)
