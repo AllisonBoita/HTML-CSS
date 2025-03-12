a0a0157ef73e69fe1cf0a546655affe8631018c9
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-03-12 08:44:35 -0300

    JAVASCRIPT: Acessando itens da lista e Propriedades

--- 13_JAVASCRIPT/ARRAYS E OBJETOS/index.js
+++ 13_JAVASCRIPT/ARRAYS E OBJETOS/index.js
@@ -1,5 +1,13 @@
 /* Arrays */
 
-const numerosPreferidos = [2,5,10,24,32]
+const numerosPreferidos = [2,3,10,24,32]
 
 console.log(numerosPreferidos)
+
+/* Acessando itens da lista */
+
+console.log(numerosPreferidos[2])
+
+/* Propriedades */
+
+console.log(numerosPreferidos.length) // Tamanho do array
